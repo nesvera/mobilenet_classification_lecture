bc196aab712b052fe5fc522e476fce065daceeb1
nesvera <daniel.nesvera@ecomp.ufsm.br>
2019-09-12 16:05:55 +0200

    normalization

--- utils/object_classes.py
+++ utils/object_classes.py
@@ -2,8 +2,6 @@
 These are the classes of objects that we are going to train the neural network
 '''
 classes = ['background', 
-           'chicken', 
-           'pedestrian', 
            'traf_sign_60', 
            'traf_sign_free', 
            'traf_sign_stop']
